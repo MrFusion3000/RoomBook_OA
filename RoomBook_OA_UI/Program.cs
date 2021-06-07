733b40ff264bedd984337cbf7e1124ab77079b02
Nico <nico@crepro.com>
2021-06-07 12:58:43 +0200

    V1.42 MudBlazor Table

--- RoomBook_OA_UI/Program.cs
+++ RoomBook_OA_UI/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using MudBlazor.Services;
 
 namespace RoomBook_OA_UI
 {
@@ -18,6 +19,7 @@ namespace RoomBook_OA_UI
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
