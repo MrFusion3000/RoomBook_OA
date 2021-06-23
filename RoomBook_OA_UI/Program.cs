23109f0018019c59dbab6c738e19f02c4c40f68d
Nico <nico@crepro.com>
2021-06-23 09:51:20 +0200

    v1.582 Authorization in frontend

--- RoomBook_OA_UI/Program.cs
+++ RoomBook_OA_UI/Program.cs
@@ -8,6 +8,8 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using RoomBook_OA_UI.AuthProviders;
 
 namespace RoomBook_OA_UI
 {
@@ -20,6 +22,9 @@ namespace RoomBook_OA_UI
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddMudServices();
+
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, TestAuthStateProvider>();
 
             await builder.Build().RunAsync();
         }
