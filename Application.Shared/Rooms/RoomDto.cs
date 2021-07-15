c35eccf5808a16737b905d740546b1becf964366
Rikard Skoog <rikard.skoog@iosoft.se>
2021-07-15 15:25:37 +0200

    Added Application.Shared

---
+++ Application.Shared/Rooms/RoomDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Domain.Entities;
+
+namespace Application.Shared.Rooms
+{
+    public class RoomDto
+    {
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+        
+        public int AnyVacantSlot { get; set; }
+        public TimeSlot FirstVacantTimeSlot { get; set; }
+    }
+}
